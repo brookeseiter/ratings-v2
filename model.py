dbe98e700a80d3ed4e79ffc1269292428fbabbc6
Brooke Seiter <bseiter@ucsd.edu>
2022-10-18 20:36:12 -0700

    Completed user/movie/rating relationship

--- model.py
+++ model.py
@@ -16,6 +16,8 @@ class User(db.Model):
     email = db.Column(db.String, unique=True)
     password = db.Column(db.String)
 
+    ratings = db.relationship("Rating", back_populates="user")
+
     def __repr__(self):
         return f'<User user_id={self.user_id} email={self.email}>'
 
@@ -31,6 +33,10 @@ class Movie(db.Model):
     overview = db.Column(db.Text)
     release_date = db.Column(db.DateTime)
     poster_path = db.Column(db.String)
+    #overview is db.Text instead of db.String bc it may end up 
+    #being a paragraph 
+
+    ratings = db.relationship("Rating", back_populates="movie")
 
     def __repr__(self):
         return f'<Movie movie_id={self.movie_id} title={self.title} >'
@@ -46,6 +52,11 @@ class Rating(db.Model):
     score = db.Column(db.Integer)
     movie_id = db.Column(db.Integer, db.ForeignKey("movies.movie_id"))
     user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
+    #foreign keys reference the movie_id attribute from movies table and 
+    #user_id attribute from users table
+
+    movie = db.relationship("Movie", back_populates="ratings")
+    user = db.relationship("User", back_populates="ratings")
 
     def __repr__(self):
         return f'<Rating rating_id={self.rating_id} score={self.score} >'
@@ -70,3 +81,25 @@ if __name__ == "__main__":
     # query it executes.
 
     connect_to_db(app)
+
+
+    # movie = db.relationship("Movie", back_populates="ratings")
+    # user = db.relationship("User", back_populates="ratings")
+    #Since a rating has a movie and a user, we added 2 attributes to 
+    #Rating — movie and user. Because of the relationship, rating.movie 
+    #will return the Movie object related to the rating and rating.user 
+    #will return the related User object. ie, "Movie" is class and 
+    #"ratings" is the current table we're in.
+
+    #The first argument in db.relationship() is the name of the class 
+    #this attribute will be associated with. The second argument is the 
+    #name of the attribute that will be used to reference the related
+    #instance(s) of this class (ex. in the example above this hint, ratings 
+    #is the name of the attribute users will have access to.) 
+    #NOTE: the second argument has the same name as the table named 
+    #ratings, but this is purely coincidence! They do not need to match.
+
+    #back_populates is assigned a value that corresponds to the name of 
+    #the attribute in the class with the relationship. In the example above 
+    #this hint, we will now be able to access the ratings attribute on User 
+    #objects because of the back_populates. 
